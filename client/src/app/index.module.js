edb4e9cb3067236ff6186d44ab8fe6b1fb492f22
vlad-velciov <velciov.vlad@gmail.com>
2017-08-20 23:46:45 +0300

    Cleanup extra components from yoeman generation

--- client/src/app/index.module.js
+++ client/src/app/index.module.js
@@ -6,10 +6,6 @@ import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { BoardsController } from '../app/boards/boards.controller';
 import { BoardController } from '../app/boards/board.controller';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 import { columnComponent } from '../app/components/column/column.component';
 import { taskComponent } from '../app/components/task/task.component';
 angular.module('sprint', [
@@ -24,20 +20,16 @@ angular.module('sprint', [
   'ui.bootstrap',
   'toastr',
   'rails'
-  ]).constant('malarkey', malarkey)
+  ])
   .constant('moment', moment)
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
   .controller('BoardsController', BoardsController)
   .controller('BoardController', BoardController)
   .component('columnComponent',  columnComponent)
   .component('taskComponent',  taskComponent)
-  .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective)
   .factory('Board', railsResourceFactory => {
     return railsResourceFactory({
       url: '/api/boards',
